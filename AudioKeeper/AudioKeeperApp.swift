462a111f12110112a0fad267ca794bd2e912d87c
rekruizer <37218220+rekruizer@users.noreply.github.com>
2025-09-24 23:26:13 +0300

    Initial Commit

---
+++ AudioKeeper/AudioKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKeeperApp.swift
+//  AudioKeeper
+//
+//  Created by Denis Yuce on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
