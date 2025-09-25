52dbbe93019502c3e4f24277b93d6c35d3b62007
rekruizer <37218220+rekruizer@users.noreply.github.com>
2025-09-25 11:41:29 +0300

    Update Homebrew Cask to v1.0.12 with correct SHA256
    - Updated version to 1.0.12
    - Added correct SHA256 hash: 7b3fa47bef487fc1ae1323ea8d6545d52927644b6d2dfdb47695b79c987e2e3c
    - Fixed URL to use AudioKeeper-v#{version}.dmg format
    - Ready for Homebrew installation

--- audiokeeper.rb
+++ audiokeeper.rb
@@ -1,8 +1,8 @@
 cask "audiokeeper" do
-  version "1.0.11"
-  sha256 "PLACEHOLDER_SHA256"
+  version "1.0.12"
+  sha256 "7b3fa47bef487fc1ae1323ea8d6545d52927644b6d2dfdb47695b79c987e2e3c"
 
-  url "https://github.com/rekruizer/AudioKeeper/releases/download/v#{version}/AudioKeeper-#{version}.dmg"
+  url "https://github.com/rekruizer/AudioKeeper/releases/download/v#{version}/AudioKeeper-v#{version}.dmg"
   name "AudioKeeper"
   desc "macOS menu bar application that automatically maintains your preferred audio input/output devices"
   homepage "https://github.com/rekruizer/AudioKeeper"
